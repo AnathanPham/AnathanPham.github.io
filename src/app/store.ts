08a44ee60859d472fb0d70d07e15333a37559dbb
黄邦胜 <sam.huang@wukongsch.com>
2023-06-14 16:55:53 +0800

    feat: middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,29 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
+import counterReducer from "../features/counter/counterSlice"
+
+const middleware =
+  ({ dispatch, getState }: any) =>
+  (next: any) =>
+  (action: any) => {
+    console.log("action:", action)
+
+    return next(action)
+  }
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
-});
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(middleware)
+  },
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
->;
+>
